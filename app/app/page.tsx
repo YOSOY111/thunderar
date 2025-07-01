8f9490b0ad29f741d99c30e28accd9c1420b4c04
YOSOY111 <irunargen@gmail.com>
2025-06-30 22:26:37 -0300

    Create page.tsx

---
+++ app/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main>
+      <h1 style={{ textAlign: "center", fontSize: 32 }}>¡Bienvenido a thunder.ar!</h1>
+      <p style={{ textAlign: "center" }}>Tu tienda mayorista de confianza</p>
+    </main>
+  );
+}
